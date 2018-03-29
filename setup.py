32d737068316a9b4d0056a422234b1f737912ca4
Artemy Lomov <lomov@restream.rt.ru>
2018-03-29 16:50:39 +0300

    Bump version to 1.0.2.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     name='foliantcontrib.epsconvert',
     description=SHORT_DESCRIPTION,
     long_description=LONG_DESCRIPTION,
-    version='1.0.1',
+    version='1.0.2',
     author='Artemy Lomov',
     author_email='artemy@lomov.ru',
     url='https://github.com/foliant-docs/foliantcontrib.epsconvert',
